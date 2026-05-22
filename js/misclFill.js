b02cbde1d0bd0ff53d45dbaa73f47ba5dc4ee235
UwaisN7 <uwaisn73@gmail.com>
2026-05-22 12:08:12 +0200

    Added the miscl page injections
    Next up is the nav heading bar with Js implementation and then i can finalise by doing a final sweep of adding images cleaning up worlding etc

---
+++ js/misclFill.js
@@ -0,0 +1,81 @@
+const blenderProjects = [
+  {
+    title: "Sword",
+    description: "First ever 3d model i made with a guide",
+    image: "../Assets/Images/sword.jpg",
+    altText: "Picture of a sword",
+  },
+  {
+    title: "Paddle Bat",
+    description: "Model made without a guide",
+    image: "../Assets/Images/paddleBat.jpg",
+    altText: "Screenshot from Project Village game",
+  },
+];
+
+const universityProjects = [
+  {
+    title: "Some PDF download",
+    description: "blablava=ah d",
+    pageLink: "",
+  },
+];
+
+function renderBlenderProjects(blenderArray, containerSelection) {
+  const container = document.getElementById(containerSelection);
+  container.innerHTML = "";
+
+  for (let i = 0; i < blenderArray.length; i++) {
+    const blenderProject = blenderArray[i];
+
+    let cardHTML = "";
+
+    if (blenderProject.pageLink && blenderProject.pageLink !== "#") {
+      cardHTML = `
+        <article class="web-container">
+          <img src="${blenderProject.image}" alt="${website.altText}">
+          <div class="card-content">
+            <h3>${blenderProject.title}</h3>
+            <p>${blenderProject.description}</p>
+            <a href="${blenderProject.webglLink}" class="play-link" target="_blank">Check out the finished page →</a>
+          </div>
+        </article>
+      `;
+    } else {
+      cardHTML = `
+        <article class="web-container">
+          <img src="${blenderProject.image}" alt="${blenderProject.altText}">
+          <div class="card-content">
+            <h3>${blenderProject.title}</h3>
+            <p>${blenderProject.description}</p>
+          </div>
+        </article>
+      `;
+    }
+
+    container.innerHTML += cardHTML;
+  }
+}
+
+function renderUniversityProjects(uniArray, containerSelection) {
+  const container = document.getElementById(containerSelection);
+  container.innerHTML = "";
+
+  for (let i = 0; i < uniArray.length; i++) {
+    const universityProject = uniArray[i];
+
+    const cardHTML = `
+      <article class="web-container">
+        <div class="card-content">
+          <h3>${universityProject.title}</h3>
+          <p>${universityProject.description}</p>
+        </div>
+      </article>
+    `;
+
+    container.innerHTML += cardHTML;
+  }
+}
+
+renderBlenderProjects(blenderProjects, "blender-container");
+renderUniversityProjects(universityProjects, "uni-container");
